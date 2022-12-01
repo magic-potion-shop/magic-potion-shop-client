1ca5eb73de578104414e3f65944197972fa64fbe
sypear <bsyeon015@gmail.com>
2022-12-01 23:25:24 +0900

    style(MainPage): 강조 문구를EmphasisText 컴포넌트로 대체

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,7 @@
 import styled from "styled-components";
 import Logo from "../../Components/Logo/Logo";
 import FillButton from "../../Components/Button/FillButton/FillButton";
-
-const Layout = styled.main`
-  color: var(--white);
-
-  & button {
-    position: absolute;
-    bottom: 25px;
-  }
-`;
+import EmphasisText from "../../Components/EmphasisText/EmphasisText";
 
 const Contents = styled.div`
   display: flex;
@@ -20,30 +12,18 @@ const Contents = styled.div`
   & img {
     margin-bottom: 18px;
   }
-
-  & p {
-    font-size: 17px;
-    font-weight: 700;
-    line-height: 24px;
-
-    & span {
-      color: #8479e1;
-    }
-  }
 `;
 
 const MainPage = () => {
   return (
-    <Layout>
+    <>
       <Contents>
         <h2 className="sr-only">마녀의 물약상점 메인 페이지</h2>
         <Logo />
-        <p>
-          어서오세요, <span>마녀의 물약상점</span>입니다
-        </p>
+        <EmphasisText>어서오세요, <span>마녀의 물약상점</span>입니다</EmphasisText>
       </Contents>
-      <FillButton contents="물약 제조하러 가기" />
-    </Layout>
+      <FillButton>물약 제조하러 가기</FillButton>
+    </>
   );
 };
 
