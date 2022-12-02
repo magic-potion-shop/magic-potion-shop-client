fcafcbfeede46f27b08ab64b983077b4948995fb
sypear <bsyeon015@gmail.com>
2022-12-02 23:17:46 +0900

    design(Line): page type에 따른 스타일 추가

--- src/components/Line/Line.jsx
+++ src/components/Line/Line.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const PAGES = {
+  menu: css`
+    width: 90%;
+    margin: 0 0 20px auto;
+    border-color: var(--line-color);
+  `,
+  letter: css`
+    width: 100%;
+    margin: 0 auto 30px auto;
+    border-color: var(--letter-line-color);
+  `
+}
 
 const Horizon = styled.hr`
-  width: 90%;
+  ${props => props.pageStyle};
   height: 1px;
   border-width: 1px 0 0 0;
   border-style: solid;
-  border-color: var(--line-color);
-  margin: 0 0 ${props => props.margin} auto;
 `;
 
-const Line = ({ margin }) => {
+const Line = ({ page = 'menu' }) => {
+  const pageStyle = PAGES[page];
+
   return (
-    <Horizon margin={margin}></Horizon>
+    <Horizon pageStyle={pageStyle} />
   );
 }
 
