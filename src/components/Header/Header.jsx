cb02ce419e992e0480362aba60a1322f572ba64b
sypear <bsyeon015@gmail.com>
2022-12-02 11:18:22 +0900

    fix: Header가 가려지는 버그 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,17 @@
 import styled from "styled-components";
-import MenuButton from "../Button/MenuButton/MenuButton";
+import BackButton from "../Buttons/BackButton/BackButton";
+import MenuButton from "../Buttons/MenuButton/MenuButton";
 
 const HeaderLayout = styled.header`
-  position: absolute;
-  top: 0;
-  right: 0;
   display: flex;
-  justify-content: right;
+  justify-content: space-between;
 `;
 
 const Header = ({ onClickMenu }) => {
   return (
     <HeaderLayout>
       <h1 className="sr-only">마녀의 물약상점</h1>
+      <BackButton />
       <MenuButton onClickMenu={onClickMenu} />
     </HeaderLayout>
   );
